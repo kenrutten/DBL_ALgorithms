f4d2b3ad46e81e1572d24db4e5c33c5b1f1ec5d7
s169713 <s169713@s169713.campus.tue.nl>
2019-04-01 13:12:37 +0200

    Implemented LabelSlider class for slider algorithm to save placement

---
+++ src/main/LabelSlider.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Ken
+ */
+public class LabelSlider extends Label{
+    // The position type of the label, can take 4 values: NORTHEAST, NORTHWEST,
+        // SOUTHEAST and SOUTHWEST.
+    float placement;
+}
